6485b8a69c8d72bca92d66258f8a952b4ad423b3
Magnus Holm <judofyr@gmail.com>
2010-01-23 17:23:25 +0100

    Remove specs for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,2 @@
-$:.unshift File.dirname(__FILE__)+"/../lib"
+$:.unshift File.dirname(__FILE__) + "/../lib"
 require 'gemify'
-
-module BaseHelper
-  def dummy
-    {
-      :name => "gemify",
-      :summary => "Just another gem",
-      :version => "0.9",
-      :author => "Magnus Holm",
-      :email => "judofyr@gmail.com",
-      :homepage => "http://dojo.rubyforge.org",
-      :rubyforge_project => "dojo",
-      :has_rdoc => true,
-      :dependencies => ["rubygems", "merb-core"]
-    }
-  end
-end
